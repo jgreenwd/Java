700147d4ca588ee615586bf09890337b3cf8a24e
Jeremy Greenwood <34409472+jgreenwd@users.noreply.github.com>
2019-02-04 12:35:35 -0600

    feat: add ex1 hello world!

---
+++ exer.java
@@ -0,0 +1,9 @@
+public class Exercises {
+    public static void ex1 {
+        System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+    // ... insert call to exercise here ...
+    }
+}
