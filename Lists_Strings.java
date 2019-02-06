6be037c712b1c68066de2a1e9b429d325b75a406
Jeremy Greenwood <34409472+jgreenwd@users.noreply.github.com>
2019-02-06 10:41:14 -0600

    refactor: add library calls & main()

--- Lists_Strings.java
+++ Lists_Strings.java
@@ -1,5 +1,12 @@
- // #1. 1. Write a function that returns the largest element in a list.
-    public static int exer(ArrayList list) {
+import java.util.Random;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class Exercises {
+ 
+/* -------- List/String programming exercises -------- */
+// #1. 1. Write a function that returns the largest element in a list.
+    public static int ex1(ArrayList list) {
         int max_ = (int) list.get(0), temp = 0;
         
         for(int i = 0; i < list.size(); i++) {
@@ -9,3 +16,9 @@
         
         return max_;
     }
+
+ /* -------- -------- -------- */
+ public static void main(String[] args) {
+        // ... insert function call to exercise here ...
+    }    
+}
