bc0701facff6fda08d2f33f169b502c4a437ef33
Jeremy Greenwood <34409472+jgreenwd@users.noreply.github.com>
2019-02-06 12:00:51 -0600

    Update Lists_Strings.java

--- Lists_Strings.java
+++ Lists_Strings.java
@@ -1,11 +1,9 @@
-import java.util.Random;
-import java.util.Arrays;
 import java.util.ArrayList;
 
 public class Exercises {
  
 /* -------- List/String programming exercises -------- */
-// #1. 1. Write a function that returns the largest element in a list.
+// #1. Write a function that returns the largest element in a list.
     public static int ex1(ArrayList list) {
         int max_ = (int) list.get(0), temp = 0;
         
@@ -17,7 +15,7 @@ public class Exercises {
         return max_;
     }
 
- /* -------- -------- -------- */
+/* ------------------------------------------------------------------------------------ */
  public static void main(String[] args) {
         // ... insert function call to exercise here ...
     }    
