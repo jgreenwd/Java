e4f2169332638fffce92747a40573412ccbc0bef
Jeremy Greenwood <34409472+jgreenwd@users.noreply.github.com>
2019-02-12 13:05:03 -0600

    Update 1.1.java

--- Introduction to Computer Science: An Interdisciplinary Approach/1.1.java
+++ Introduction to Computer Science: An Interdisciplinary Approach/1.1.java
@@ -35,12 +35,12 @@ public class UseArgument {
     e. java UseArgument Alice Bob   - prints "Alice" to screen
    */
 }
+  
+public class UseThree {
 
   // 1.1.6 Modify UseArgument.java to make a program UseThree.java that takes three names and prints out a proper 
   // sentence with the names in the reverse of the order given, so that, for example, java UseThree Alice Bob Carol 
   // gives Hi Carol, Bob, and Alice.
-  
-public class UseThree {
   public static void main(String[] args) {
       System.out.println("Hi, " + args[2] + ", " + args[1] + ", and " + args[0] + ".");
   }
