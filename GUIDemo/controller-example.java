c2dae7343cfb1c52e79cf4d21f3ee36185ddffbd
Jeremy Greenwood <34409472+jgreenwd@users.noreply.github.com>
2019-02-13 21:55:35 -0600

    feat: added combobox example

--- GUIDemo/controller-example.java
+++ GUIDemo/controller-example.java
@@ -7,26 +7,17 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
 public class FXMLDocumentController implements Initializable {
     
-    // These items for CheckBox example
+    /* ---------- CheckBox example ---------- */
     @FXML private Label pizzaOrderLabel;
     @FXML private CheckBox pepperoniCheckBox;
     @FXML private CheckBox pineappleCheckBox;
     @FXML private CheckBox baconCheckBox;
     
-    // These items for ChoiceBox example
-    @FXML private ChoiceBox choiceBox;
-    @FXML private Label choiceBoxLabel;
-    
-    /* This will update ChoiceBox example */
-    public void choiceBoxButtonPushed() {
-        choiceBoxLabel.setText("My favorite fruit is: " + choiceBox.getValue().toString());
-    }
-    
-    /* This will update CheckBox example */
     public void pizzaOrderButtonPushed() {
         String order = "Toppins are:";
         if (pepperoniCheckBox.isSelected())
@@ -40,18 +31,36 @@ public class FXMLDocumentController implements Initializable {
     }
 
     
-    private void handleButtonAction(ActionEvent event) {
-        pizzaOrderLabel.setText("stuff");
+    /* ---------- ChoiceBox example ---------- */
+    @FXML private ChoiceBox choiceBox;
+    @FXML private Label choiceBoxLabel;
+    
+    public void choiceBoxButtonPushed() {
+        choiceBoxLabel.setText("My favorite fruit is: " + choiceBox.getValue().toString());
+    }
+    
+
+    /* ----------- ComboBox example ---------- */
+    @FXML private ComboBox comboBox;
+    @FXML private Label comboBoxLabel;
+    
+    public void comboBoxWasUpdated() {
+        this.comboBoxLabel.setText("Course selected: \n" + comboBox.getValue().toString());
     }
         
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        // CheckBox example init
         pizzaOrderLabel.setText("");
         
-        // These are for ChoiceBox example
+        // ChoiceBox example init
         choiceBoxLabel.setText("");
         choiceBox.getItems().addAll("apples","oranges","pairs", "bananas", "strawberries");
         choiceBox.setValue("apples");
+        
+        // ComboBox example init
+        comboBox.getItems().add("C462");
+        comboBox.getItems().addAll("C190", "C191", "C960");
     }    
     
 }
