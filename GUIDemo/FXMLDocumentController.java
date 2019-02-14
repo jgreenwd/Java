9cfe91dc4d3e753dc090ce715bd071a2c9bcab27
Jeremy Greenwood <34409472+jgreenwd@users.noreply.github.com>
2019-02-14 17:10:27 -0600

    feat: add Scene change button

--- GUIDemo/FXMLDocumentController.java
+++ GUIDemo/FXMLDocumentController.java
@@ -1,11 +1,16 @@
 package guidemo;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
@@ -15,6 +20,7 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
 
 public class FXMLDocumentController implements Initializable {
     
@@ -91,9 +97,20 @@ public class FXMLDocumentController implements Initializable {
     }
     
     
+    /* ---------- Scene Change example ---------- */
+    public void changeSceneButton(ActionEvent event) throws IOException {
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("ExampleTableView.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
+        
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(tableViewScene);
+        window.show();
+    }
+    
     
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    public void initialize(URL url, ResourceBundle rb) { 
         // CheckBox example init
         pizzaOrderLabel.setText("");
         
